7831e8625fdda3f42cccaeb7971e5efe85169c42
김영은 <yevelop10@gmail.com>
2025-02-21 11:33:42 +0900

    #17 - 스프링 시큐리티 인증 Config
    아까 추가하지 못한 내용 다시 추가 (dependency를 한 번에 추가하지 않아서!!!)

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -2,15 +2,19 @@ package com.fastcampus.projectboard.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return  http
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin()
+                .formLogin(withDefaults())
                 .build();
     }
 
