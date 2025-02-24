07ce3dbab61effa2befe51db6f8c243bcc6db75d
김영은 <yevelop10@gmail.com>
2025-02-24 11:06:19 +0900

    도메인 수정 - 회원 계정과 게시글, 댓글 관계 연결
    회원 계정 엔티티와 게시글, 댓글 엔티티 연관관계 업데이트

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashTag"),
@@ -23,27 +23,30 @@ public class Article extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount;   // 유저 정보 (ID)
+
     @Setter @Column(nullable = false) private String title;
     @Setter @Column(nullable = false, length = 10000) private String content;
 
     @Setter private String hashTag;
 
-    @OrderBy("id")
-    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     @ToString.Exclude
+    @OrderBy("createdAt DESC")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
 
     protected Article() {}
 
-    private Article(String title, String content, String hashTag) {
+    private Article(UserAccount userAccount, String title, String content, String hashTag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashTag = hashTag;
     }
 
-    public static Article of(String title, String content, String hashTag) {
-        return new Article(title, content, hashTag);
+    public static Article of(UserAccount userAccount, String title, String content, String hashTag) {
+        return new Article(userAccount, title, content, hashTag);
     }
 
     @Override
